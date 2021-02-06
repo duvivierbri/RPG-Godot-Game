011efb58ea8e8d61758312acd43519afb9e943bf
DuvivierBri <47341822+duvivierbri@users.noreply.github.com>
2021-02-06 03:30:47 -0500

    Update World.gd

--- World.gd
+++ World.gd
@@ -1,14 +1,13 @@
 extends Node2D
 var touchingGreen = false
-var nearCoffee = false
 var spokenToGreen = false
-var missionComplete = false
+var missionComplete = false #Checks if level is complete
 var stickObtained = false
 var ballObtained = false
 var touchingTree = false
-var ballisInTree = true
+var ballisInTree = true #Chekcs if player has completed part of the level
 
-func _ready():
+func _ready(): #Setting up the scene
 	$sign.visible = false
 	$GreenBody2/Music.play()
 	$Cloud1/AnimationTree/AnimationPlayer.play("glide")
@@ -36,7 +35,7 @@ func _process(delta):
 					var green_dialog2 = Dialogic.start('GreensProblem2')
 					add_child(green_dialog2)
 
-func walkingCheck():
+func walkingCheck(): #This allows an animation to play when the character moves
 	if Input.is_action_pressed("ui_right"):
 		$BlueBody/Blue/AnimationTree/AnimationPlayer.set_current_animation("Walk")
 	elif Input.is_action_pressed("ui_left"):
@@ -48,13 +47,13 @@ func walkingCheck():
 	else:
 		$BlueBody/Blue/AnimationTree/AnimationPlayer.set_current_animation("Idle")
 
-func _on_greenArea2_area_entered(area):
+func _on_greenArea2_area_entered(area): #Checks to see if the main character is touching the Green character and prompts them to speak to them
 	$BlueBody/hint.visible = true
 	touchingGreen = true
 	$BlueBody/hint.bbcode_text = "Press Z to interact"
 
 
-func _on_greenArea2_area_exited(area):
+func _on_greenArea2_area_exited(area): #Checks to see if the player isnt touching the Green character
 	$BlueBody/hint.visible = false
 	touchingGreen = false
 
@@ -79,7 +78,7 @@ func _on_treeArea_area_exited(area):
 	touchingTree = false
 	$BlueBody/hint.visible = false
 
-func getBallCheck():
+func getBallCheck(): #This code checks to see if the player has gotten the stick before interacting with the tree. The dialog is different depending on whether they got it yet
 	if Input.is_key_pressed(KEY_Z):
 		if touchingTree == true:
 			if stickObtained == true:
@@ -94,7 +93,7 @@ func getBallCheck():
 					var shakeTree = Dialogic.start('ShakeTree')
 					add_child(shakeTree)
 					
-func greenResolutionCheck():
+func greenResolutionCheck(): #This checks to see if they've completed the challenge for this level 
 	if Input.is_key_pressed(KEY_Z):
 		if touchingGreen == true:
 			if ballObtained == true:
@@ -102,11 +101,11 @@ func greenResolutionCheck():
 				add_child(greenResolution)
 				greenResolution.connect("dialogic_signal", self, 'openNextScene')
 
-func openNextScene(value):
+func openNextScene(value): #What happens when Dialogic signal emits from this dialog
 	get_node("rightBoarder/rightCollision").disabled = true
 	$sign.visible = true
 
-func _on_button_pressed():
+func _on_button_pressed(): #Allows player to obtain ball after poking it out of the tree
 	if ballisInTree == false:
 		$pickup.play()
 		$Ball.hide()
@@ -114,5 +113,5 @@ func _on_button_pressed():
 		var ballDialog = Dialogic.start('BallObtained')
 		add_child(ballDialog)
 
-func _on_changeScene_area_entered(area):
+func _on_changeScene_area_entered(area): #Go to next scene when they move far enough to the right
 	get_tree().change_scene("res://Quest2.tscn")
